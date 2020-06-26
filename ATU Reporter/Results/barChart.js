397920d9a2e906f559652c157bbf7b3fa30634c8
Mullai <mullaimc@gmail.com>
2020-06-26 22:39:41 +0530

    Comitting changes on ReadMe file

--- ATU Reporter/Results/barChart.js
+++ ATU Reporter/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [1,0,0,0,0,0,0,0,0,1];
-                var s2 = [0,1,1,1,1,1,1,1,1,0];
+                var s1 = [0,0,0,0,0,0,0,0,1,1];
+                var s2 = [1,1,1,1,1,1,1,1,0,0];
                 var s3 = [0,0,0,0,0,0,0,0,0,0];
 
-var ticks = [17,18,19,20,21,22,23,24,25,26];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [18,19,20,21,22,23,24,25,26,27];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 1},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SeleniumTestNG
+
+
+-Uses TestNG to trigger and maintain test cases
+-Excel and Properties files to store test data
+-ATU for report capturing
+#Project.properties - has site urls and browser details
